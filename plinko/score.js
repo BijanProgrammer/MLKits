ca3b22cfc15804e2b3412faa1de7387cf7b534ca
BijanProgrammer <bijaneisapour@gmail.com>
2024-07-31 22:29:31 +0330

    [ADD] npm and vite

--- plinko/score.js
+++ plinko/score.js
@@ -1,16 +1,36 @@
-const outputs = [];
-const predictionPoint = 300;
-const k = 3;
+import _ from "lodash";
 
-function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+const outputs = [];
+const testSize = 50;
+
+export function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
-function runAnalysis() {
+export function runAnalysis() {
   // Write code here to analyze stuff
-  const bucket = _.chain(outputs)
-    .map((row) => [distance(row[0]), row[3]])
+  const [testSet, trainingSet] = splitDataset(outputs, testSize);
+
+  _.range(1, 15, 1).forEach((k) => {
+    const accuracy = _.chain(testSet)
+      .filter(
+        (testPoint) =>
+          knn(trainingSet, _.initial(testPoint), k) === testPoint[3],
+      )
+      .size()
+      .divide(testSize)
+      .value();
+
+    console.log(`Accuracy (k=${k}): ${accuracy}`);
+  });
+}
+
+function knn(data, point, k) {
+  return _.chain(data)
+    .map((row) => {
+      return [distance(_.initial(row), point), _.last(row)];
+    })
     .sortBy((row) => row[0])
     .slice(0, k)
     .countBy((row) => row[1])
@@ -20,10 +40,23 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value();
-
-  console.log(`Predicted Bucket: ${bucket}`);
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  );
+}
+
+function splitDataset(data, testSize) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testSize);
+  const trainingSet = _.slice(shuffled, testSize);
+
+  return [testSet, trainingSet];
 }
