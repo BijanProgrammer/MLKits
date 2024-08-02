98c55555f2bdf4d81f33a50686c89be3544fc619
BijanProgrammer <bijaneisapour@gmail.com>
2024-08-02 07:36:53 +0330

    [UPDATE] using real data

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,29 +1,70 @@
 const tf = require("@tensorflow/tfjs-node");
+const loadCsv = require("./load-csv");
 
-const k = 2;
+function knn(features, labels, predictionPoint, k) {
+  const { mean, variance } = tf.moments(features, 0);
 
-const features = tf.tensor([
-  [-121, 47],
-  [-121.2, 46.5],
-  [-122, 46.4],
-  [-120.9, 46.7],
-]);
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
 
-const labels = tf.tensor([[200], [250], [215], [240]]);
+  return (
+    features
+      .sub(mean)
+      .div(variance.pow(0.5))
+      .sub(scaledPrediction)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => a.arraySync()[0] - b.arraySync()[0])
+      .slice(0, k)
+      .reduce((sum, t) => sum + t.arraySync()[1], 0) / k
+  );
+}
 
-const predictionPoint = tf.tensor([-121, 47]);
+function main() {
+  const k = 10;
 
-const result =
-  features
-    .sub(predictionPoint)
-    .pow(2)
-    .sum(1)
-    .pow(0.5)
-    .expandDims(1)
-    .concat(labels, 1)
-    .unstack()
-    .sort((a, b) => a.arraySync()[0] - b.arraySync()[0])
-    .slice(0, k)
-    .reduce((sum, t) => sum + t.arraySync()[1], 0) / k;
+  let { features, labels, testFeatures, testLabels } = loadCsv(
+    "kc_house_data.csv",
+    {
+      shuffle: true,
+      splitTest: 10,
+      dataColumns: [
+        "lat",
+        "long",
+        "sqft_lot",
+        "sqft_living",
+        "yr_built",
+        "condition",
+        "grade",
+      ],
+      labelColumns: ["price"],
+    },
+  );
 
-console.log(result);
+  features = tf.tensor(features);
+  labels = tf.tensor(labels);
+
+  testFeatures.forEach((testPoint, testIndex) => {
+    const predictionPoint = tf.tensor(testPoint);
+    const result = knn(features, labels, predictionPoint, k);
+
+    const error =
+      Math.floor(
+        (Math.abs(testLabels[testIndex][0] - result) /
+          testLabels[testIndex][0]) *
+          100 *
+          100,
+      ) / 100;
+
+    console.log({
+      calculated: result,
+      actual: testLabels[testIndex][0],
+      accuracy: `${100 - error}%`,
+    });
+  });
+}
+
+main();
